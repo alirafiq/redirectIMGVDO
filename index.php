17d9c5e6756b95c0b35e98724eaf37afc7e05e46
alirafiq <ali.almaas@gmail.com>
2016-05-12 00:44:03 +0500

    change header

--- index.php
+++ index.php
@@ -24,10 +24,10 @@ $newfile = 'my.mp4';
 
 if (file_exists($file)) {
     header('Content-Description: File Transfer');
-    header('Content-Type: application/mp4');
+    header('Content-Type: application/octet-stream');
     header('Content-Disposition: attachment; filename='.basename($newfile));
     header('Expires: 0');
-//    header('Cache-Control: must-revalidate');
+    header('Cache-Control: must-revalidate');
     header('Pragma: public');
     header('Content-Length: ' . filesize($file));
     ob_clean();
