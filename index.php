790c37aea3c34fdc73791e96bd346c598189e50f
alirafiq <ali.almaas@gmail.com>
2016-05-11 23:04:26 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+$file = 'image.jpg';
+//$filename = basename($file);
+//$file_extension = strtolower(substr(strrchr($filename,"."),1));
+
+switch( $file_extension ) {
+    case "gif": $ctype="image/gif"; break;
+    case "png": $ctype="image/png"; break;
+    case "jpeg":
+    case "jpg": $ctype="image/jpeg"; break;
+    default:
+}
+
+
+//header('Content-type: ' . $ctype);
+//readfile($file);
+
+
+?>
+
+<?php
+$file = 'my.mp4';
+$newfile = 'my.mp4';
+
+if (file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/mp4');
+    header('Content-Disposition: attachment; filename='.basename($newfile));
+    header('Expires: 0');
+//    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    ob_clean();
+    flush();
+    readfile($file);
+    exit;
+}
+die();
+
+  ?>
